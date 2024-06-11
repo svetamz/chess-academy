f4e48753e13348267f05766d60762d6fa8fa4c9a
vladarsky <nikita.vladarsky@koyfin.com>
2024-06-11 14:17:08 +0300

    save

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {Intro} from "./components/Into.tsx";
+import {Benefits} from "./components/Benefits.tsx";
+import {Lessons} from "./components/Lessons.tsx";
+import {Teachers} from "./components/Teachers.tsx";
+import {Advantages} from "./components/Advantages.tsx";
+import {Contacts} from "./components/Contacts.tsx";
+
+
+function App() {
+    return (
+        <>
+            <Intro></Intro>
+            <Benefits></Benefits>
+            <Lessons></Lessons>
+            <Advantages></Advantages>
+            <Teachers></Teachers>
+            <Contacts></Contacts>
+        </>
+    )
+}
+
+export default App
